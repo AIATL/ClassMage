82bf6173fafc1957df15e0e729a83dfeccf8b246
Theo <theomhalpern@gmail.com>
2024-10-26 17:31:12 -0400

    changed footer

--- src/components/HeaderFooter.jsx
+++ src/components/HeaderFooter.jsx
@@ -25,12 +25,11 @@ const HeaderFooter = ({ children }) => {
                 <div>ClassMage</div>
                 <Link to={"/classes"}>My Classes</Link>
                 <Button color="dark" onClick={handleLogout}>LOG OUT</Button>
-                <div>header</div> {/* Placeholder for actual header content */}
             </header>
             <Notifications position="top-right" />
             <main className="flex-grow">{children}</main>
             <footer className="fixed bottom-0 w-full h-[50px] bg-[#6c3adb] flex items-center justify-center text-white">
-                footer
+                Like us on <a href="">DevPost</a>
             </footer>
         </>
     );
