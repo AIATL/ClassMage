48942c1958d24f3386d3d8af9172a1b6499f6b31
Theo <theomhalpern@gmail.com>
2024-10-26 20:14:49 -0400

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 
 load_dotenv(os.environ.get("GOOGLE_CLOUD_JSON"))
 
-json_account_info = json.loads()  # convert JSON to dictionary
+json_account_info = json.loads(os.environ.get("GOOGLE_CLOUD_JSON"))  # convert JSON to dictionary
 credentials = service_account.Credentials.from_service_account_info(
     json_account_info)
 
